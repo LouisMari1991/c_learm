0b7d6eb8ddb08ac1d4f05670c8389111d731b520
Mari <Marishunxiang@gmail.com>
2018-08-05 22:10:55 +0800

    ref parameters

--- filefunc.cpp
+++ filefunc.cpp
@@ -1,1 +1,55 @@
-// filefunc.cpp --
+// filefunc.cpp -- function with ostream & parameter
+#include<iostream>
+#include<fstream>
+#include<cstdlib>
+using namespace std;
+
+void file_it(ostream &os, double fo, const double fe[], int n);
+const int LIMIT = 5;
+
+int main()
+{
+    ofstream fout;
+    const char *fn = "ep-data.txt";
+    fout.open(fn);
+    if (!fout.is_open())
+    {
+        cout << "Can't open " << fn << ". Bye.\n";
+        exit(EXIT_FAILURE);
+    
+    }
+    double objective;
+    cout << "Enter the focal length of your "
+        "telescope objective in mm: ";
+    cin >> objective;
+    double eps[LIMIT];
+    cout << "Enter the focal lengths, in mm, of " << LIMIT << " eyeieces:\n";
+    for (int i = 0; i < LIMIT; i++)
+    {
+        cout << "Eyepice #" << i + 1 << ": ";
+        cin >> eps[i];
+    }
+    file_it(fout, objective, eps, LIMIT);
+    file_it(cout, objective, eps, LIMIT);
+    cout << "Done\n";
+    return 0;
+}
+
+void file_it(ostream &os, double fo, const double fe[], int n)
+{
+    ios_base::fmtflags initial;
+    initial = os.setf(ios_base::fixed); // save initial formating sate
+    os.precision(0);
+    os << "Focal length of objective: " << fo << " mm\n";
+    os.setf(ios::showpoint);
+    os.width(12);
+    os << "magnification" << endl;
+    for (int i = 0; i < n; i ++)
+    {
+        os.width(12);
+        os << fe[i];
+        os.width(15);
+        os << int (fo/fe[i]) << endl;
+    }
+    os.setf(initial);   // restore initial formating state
+}
